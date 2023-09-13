43e9016df58912f8980d13521c309ca681cd9624
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-09-13 15:47:02 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -32,3 +32,37 @@ class FizzBuzz {
         }
     }
 }
+
+
+class FizzBuzzW {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i < 100) {
+
+
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 && divisibleBy5) {
+
+                System.out.println("Fizz Buzz");
+
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+
+            }
+        i++;
+        }
+    }
+}
