4a1453ec3315e1eeee2c6b3090c2c99e1dbbd233
Alexa <alexahs.zhang@mail.utoronto.ca>
2023-09-13 17:22:58 -0400

    include and public doFizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -30,39 +30,39 @@ class FizzBuzz {
 
             }
         }
-    }
-}
-
-
-class FizzBuzzW {
-
-    public static void main(String[] args) {
 
         int i = 1;
         while (i < 100) {
 
 
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-        i++;
+            i = WFizzBuzz(i);
         }
     }
+
+    private static int WFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
+    }
 }
+
+
